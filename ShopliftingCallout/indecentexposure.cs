75729ddea42e063e467b29ed6455609756f10f26
Shplink <jwalker9857@gmail.com>
2020-06-27 15:28:48 -0500

    Updated for FivePD v1.1

--- ShopliftingCallout/indecentexposure.cs
+++ ShopliftingCallout/indecentexposure.cs
@@ -5,15 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using CitizenFX.Core;
 using static CitizenFX.Core.Native.API;
-using CalloutAPI;
+using FivePD.API;
 /* This callout is one of the first callouts that I have made. I also consider this open source. If anybody wants to offer suggestions or add to the code...
  * I would be more than accepting of it. */
 
 
 namespace IndecentExposure
 {
-    [CalloutProperties("IndecentExposure","Shplink","1.0",Callout.Probability.Medium)]
-    public class IndecentExposure : CalloutAPI.Callout
+    [CalloutProperties("IndecentExposure", "Shplink", "1.1")]
+    public class IndecentExposure : FivePD.API.Callout
     {
 
         Ped Suspect, Victim;
@@ -26,7 +26,7 @@ namespace IndecentExposure
             float offsetX = random.Next(100, 600);
             float offsetY = random.Next(100, 600);
 
-            InitBase(World.GetNextPositionOnStreet(Game.PlayerPed.GetOffsetPosition(new Vector3(offsetX, offsetY, 0))));
+            InitInfo(World.GetNextPositionOnStreet(Game.PlayerPed.GetOffsetPosition(new Vector3(offsetX, offsetY, 0))));
 
             this.ShortName = "Indecent Exposure";
             this.CalloutDescription = "A report of somebody indecently exposing themselves has come in. Respond Code 1.";
@@ -34,9 +34,9 @@ namespace IndecentExposure
             this.StartDistance = 30f;
         }
 
-        public async override Task Init()
+        public async override Task OnAccept()
         {
-            OnAccept();
+            this.InitBlip();
 
             Random random = new Random();
 
